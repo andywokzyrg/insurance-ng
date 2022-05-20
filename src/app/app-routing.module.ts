4a59db2ce6e9c8c526bc5a438e322d5941c91b47
andywokzyrg <andywokzyrg>
2022-05-20 21:49:20 +0300

    main-layout added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainLayoutComponent} from "./shared/main-layout/main-layout.component";
+import {ProductPageComponent} from "./product-page/product-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '', component: MainLayoutComponent, children: [
+            {path: '', redirectTo: '/', pathMatch: 'full'},
+            {path: '', component: ProductPageComponent},
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ButtonComponent } from './shared/ui/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    ProductPageComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
